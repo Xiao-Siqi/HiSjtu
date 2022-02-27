bef67ee121f6ca9b31c950cf21b9ceb604fe431d
Xiao-Siqi <xiaosiqi2000@sjtu.edu.cn>
2022-02-27 13:51:17 +0800

    second submit

---
+++ app/src/main/java/com/bytedance/hisjtu/MainActivity2.kt
@@ -0,0 +1,11 @@
+package com.bytedance.hisjtu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity2 : AppCompatActivity() {//继承谷歌模版
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)//指定页面需要什么布局
+    }
+}
